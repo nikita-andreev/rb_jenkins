86d59f139051f252a4669185b486d43fa0b54d03
Nikita Andreyev <msa31415@gmail.com>
2018-11-08 18:45:50 +0200

    visual test

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 
 gem 'rake'
 gem 'rspec'
+gem 'eyes_selenium'
---
+++ spec/visual_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'eyes_selenium'
+
+RSpec.describe 'GITHUB' do
+  it 'index page' do
+    begin
+      desired_caps = Selenium::WebDriver::Remote::Capabilities.chrome
+      desired_caps[:username] = ENV['SAUCE_USERNAME']
+      desired_caps[:accesskey] = ENV['SAUCE_ACCESS_KEY']
+      driver = Selenium::WebDriver.for :remote, url: 'http://ondemand.saucelabs.com/wd/hub', desired_capabilities: desired_caps
+      eyes = Applitools::Selenium::Eyes.new
+      eyes.api_key = ENV['APPLITOOLS_API_KEY']
+      eyes.log_handler = Logger.new(STDOUT).tap do |l|
+        l.level = Logger::FATAL
+      end
+    
+      eyes_driver = eyes.open(driver: driver, app_name: 'MS Azure DevOps', test_name: 'Github index page', viewporft_size: {width: 800, height: 600})
+      eyes_driver.get('http://github.com')
+      eyes.check_window('window')
+    rescue StandardError => e
+      p e.message
+      eyes.close
+    end
+  end 
+end
